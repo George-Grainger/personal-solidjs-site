ed274fa9f2a17dbb817ece377240518bc6929238
u87612gg <george.grainger-3@student.manchester.ac.uk>
2022-09-18 18:54:10 +0100

    Add close button to spotify cards

--- src/components/Card/TopTrackCard.tsx
+++ src/components/Card/TopTrackCard.tsx
@@ -4,15 +4,28 @@ import { Audio } from './Audio';
 import styles from './Card.module.css';
 import { SpotifyLogo } from './Logos';
 
-const [autoPlay, setAutoPlay] = createSignal(true);
+const [autoPlay, setAutoPlay] = createSignal(false);
 
 // Not having browser support to style the HTML audio is such a pain
 export const TopTrackCard: VoidComponent<TopTrack> = (props) => {
   let parent: HTMLDivElement | undefined;
+  const audioId = props.title
+    .toLowerCase()
+    .replaceAll(/[(),.'"%$&#!\\\/]/g, '')
+    .replaceAll(' ', '-');
+
+  const handleClose = () => {
+    const nextCard = parent?.querySelector(`#${audioId} ~ * button:last-of-type`) as HTMLButtonElement | null;
+    nextCard?.focus();
+    nextCard?.blur();
+  };
 
   return (
     <div ref={parent} class={styles.topTrackCard} tabindex={0} role="gridcell">
       <img src={props.imgUrl} alt={`Album image for ${props.title}`} loading="lazy" />
+      <svg tabindex={0} class={styles.closeButton} onClick={handleClose} role="button" viewBox="0 0 24 24">
+        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+      </svg>
       <div class={styles.topTrackdetails}>
         <p class={styles.songTitle}>{props.title}</p>
         <a class={styles.spotifyLink} href={props.playUrl} target="_blank" rel="noopener noreferrer">
@@ -23,10 +36,7 @@ export const TopTrackCard: VoidComponent<TopTrack> = (props) => {
         <Show when={props.previewUrl}>
           <Audio
             src={props.previewUrl}
-            id={props.title
-              .toLowerCase()
-              .replaceAll(/[(),.'"%$!\\\/]/g, '')
-              .replaceAll(' ', '-')}
+            id={audioId}
             parentOptions={{ parentRef: parent, setAutoPlay, playOnFocus: autoPlay, fadeOutOnFocusOut: () => true }}
           />
         </Show>
